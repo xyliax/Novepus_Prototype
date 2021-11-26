02d41d5f0309bf5d3b3321476cf9ccc4d1a1a270
pyx-2021 <20083743d@connect.polyu.hk>
2021-11-26 21:40:51 +0800

    model

--- src/controller/DBController.java
+++ src/controller/DBController.java
@@ -1,58 +1,56 @@
 package controller;
 
-import java.io.*;
-import java.io.Console;
+import model.Post;
+import model.User;
+import oracle.jdbc.driver.OracleConnection;
+import oracle.jdbc.driver.OracleDriver;
+
 import java.sql.*;
 import java.util.ArrayList;
 
-import model.*;
-import oracle.jdbc.driver.*;
-import oracle.sql.*;
-
 public class DBController {
-    static OracleConnection conn ;
-    public static void createUser(User user) throws SQLException{
+    static OracleConnection conn;
+
+    public static void createUser(User user) throws SQLException {
         Date date = new Date(new java.util.Date().getTime());
         Statement stmt = conn.createStatement();
         ResultSet rset = stmt.executeQuery(
-                String.format("INSERT INTO USER VALUES %s,%s,%s,%s,%s",,"null",user.userName(),user.userPassword(),user.userEmail(),date.toString(),"null","0")
+                String.format("INSERT INTO USER VALUES null,%s,%s,%s,%s,null,0", user.userName(), user.userPassword(), user.userEmail(), date)
         );
 
     }
 
 
     // ---------------------
-    public static void addUserInterest(String username,String labelName){
+    public static void addUserInterest(String username, String labelName) {
 
     }
 
-    public static User retrieveUserById(int userId){
+    public static User retrieveUserById(int userId) {
         return null;
     }
 
-    public static User retrieveUserByName(String userName){
+    public static User retrieveUserByName(String userName) {
         return null;
     }
 
-    public static Post retrievePostById(int postId){
+    public static Post retrievePostById(int postId) {
         return null;
     }
 
-    public static ArrayList<Post> searchPostByKey(String keyword){
+    public static ArrayList<Post> searchPostByKey(String keyword) {
         return null;
     }
 
-    public static ArrayList<String> getUserInterest(String username){
+    public static ArrayList<String> getUserInterest(String username) {
         return null;
     }
 
 
-
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try {
             DriverManager.registerDriver(new OracleDriver());
-            conn = (OracleConnection)DriverManager.getConnection("jdbc:oracle:thin:@studora.comp.polyu.edu.hk:1521:dbms","20075519D","viukiyec");
+            conn = (OracleConnection) DriverManager.getConnection("jdbc:oracle:thin:@studora.comp.polyu.edu.hk:1521:dbms", "20075519D", "viukiyec");
         } catch (SQLException e) {
             e.printStackTrace();
         }
