2b0ccd6dc8e546955633607cd2bc5b927768867e
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2021-11-26 21:36:50 +0800

    Update DBController.java

--- src/controller/DBController.java
+++ src/controller/DBController.java
@@ -1,9 +1,61 @@
 package controller;
 
-
+import java.io.*;
+import java.io.Console;
+import java.sql.*;
 import java.util.ArrayList;
 
-public class DBController {
+import model.*;
+import oracle.jdbc.driver.*;
+import oracle.sql.*;
 
-    public static ArrayList<String>
+public class DBController {
+    static OracleConnection conn ;
+    public static void createUser(User user) throws SQLException{
+        Date date = new Date(new java.util.Date().getTime());
+        Statement stmt = conn.createStatement();
+        ResultSet rset = stmt.executeQuery(
+                String.format("INSERT INTO USER VALUES %s,%s,%s,%s,%s",,"null",user.userName(),user.userPassword(),user.userEmail(),date.toString(),"null","0")
+        );
+
+    }
+
+
+    // ---------------------
+    public static void addUserInterest(String username,String labelName){
+
+    }
+
+    public static User retrieveUserById(int userId){
+        return null;
+    }
+
+    public static User retrieveUserByName(String userName){
+        return null;
+    }
+
+    public static Post retrievePostById(int postId){
+        return null;
+    }
+
+    public static ArrayList<Post> searchPostByKey(String keyword){
+        return null;
+    }
+
+    public static ArrayList<String> getUserInterest(String username){
+        return null;
+    }
+
+
+
+
+    public static void main(String[] args){
+        try {
+            DriverManager.registerDriver(new OracleDriver());
+            conn = (OracleConnection)DriverManager.getConnection("jdbc:oracle:thin:@studora.comp.polyu.edu.hk:1521:dbms","20075519D","viukiyec");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
