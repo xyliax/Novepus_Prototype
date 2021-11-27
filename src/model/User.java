52d0d2f9dab20d90882ef9ea3d04af0506d57f24
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2021-11-28 01:18:48 +0800

    update

--- src/model/User.java
+++ src/model/User.java
@@ -2,9 +2,7 @@ package model;
 
 import controller.DBController;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
 
 public final record User(int userId, String userName, String userPassword, String userEmail,
                          boolean online, String regDate, String exitDate,
@@ -12,27 +10,26 @@ public final record User(int userId, String userName, String userPassword, Strin
                          ArrayList<Integer> postIdList,
                          ArrayList<Integer> followingsIdList,
                          ArrayList<Integer> followersIdList) {
+    public User(String userName, String userPassword, String userEmail){
+        this(0, userName, userPassword, userEmail, false, null, null, null, null, null, null);
+    }
     @Override
     public String toString() {
-        try {
-            return String.format("""
-                            ________________________________________________
-                            [    User_Information______%s
-                            [        username________| %s
-                            [        email___________| %s
-                            [        register_date___| %s
-                            [        last_online_____| %s
-                            [            posts_______| %d
-                            [            followings__| %d
-                            [            followers___| %d
-                            [            interested__| %s
-                            ------------------------------------------------
-                            """, online ? "ONLINE" : "OFFLINE", userName, userEmail,
-                    regDate, online ? "NOW" : exitDate,
-                    postIdList.size(), followingsIdList.size(), followersIdList.size(),
-                    DBController.getUserInterest(userId));
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        return String.format("""
+                        ________________________________________________
+                        [    User_Information______%s %s
+                        [        username________| %s
+                        [        email___________| %s
+                        [        register_date___| %s
+                        [        last_online_____| %s
+                        [            posts_______| %d
+                        [            followings__| %d
+                        [            followers___| %d
+                        [            interested__| %s
+                        ------------------------------------------------
+                        """, online ? "ONLINE" : "OFFLINE", userId, userName, userEmail,
+                regDate, online ? "NOW" : exitDate,
+                postIdList.size(), followingsIdList.size(), followersIdList.size(),
+                DBController.getUserInterest(userId));
     }
 }
